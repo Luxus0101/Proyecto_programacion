615060136370d87c76f0eacb790392b7fb6065d5
Lucas Piorno <lucas.piorno@estudiante.uam.es>
2021-04-23 16:02:56 +0200

    Added some changes to documentation.txt Added doxygen info Completed I3

---
+++ doc/html/search/files_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['links_2ec_88',['links.c',['../links_8c.html',1,'']]]
+];
---
+++ doc/html/search/searchdata.js
@@ -0,0 +1,27 @@
+var indexSectionsWithContent =
+{
+  0: "_cdglst",
+  1: "_",
+  2: "cdglst",
+  3: "dgls",
+  4: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "typedefs"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Typedefs"
+};
+
